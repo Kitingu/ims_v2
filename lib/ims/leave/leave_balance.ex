1f5d6eeea3e75077b355e86df998e4815fa1c329
kitingu <benlegendj@gmail.com>
2025-04-04 20:10:27 +0300

    setup profile

--- lib/ims/leave/leave_balance.ex
+++ lib/ims/leave/leave_balance.ex
@@ -1,19 +1,83 @@
 defmodule Ims.Leave.LeaveBalance do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
+  alias Ims.Repo
+  # import Ims.RepoHelpers, repo: Repo
 
   schema "leave_balances" do
-    field :remaining_days, :integer
-    field :user_id, :id
-    field :leave_type_id, :id
+    field :remaining_days, :decimal
+    belongs_to :user, Ims.Accounts.User
+    belongs_to :leave_type, Ims.Leave.LeaveType
 
     timestamps(type: :utc_datetime)
   end
 
-  @doc false
   def changeset(leave_balance, attrs) do
     leave_balance
-    |> cast(attrs, [:remaining_days])
-    |> validate_required([:remaining_days])
+    |> cast(attrs, [:remaining_days, :user_id, :leave_type_id])
+    |> validate_required([:remaining_days, :user_id, :leave_type_id])
+    |> validate_number(:remaining_days,
+      greater_than_or_equal_to: 0,
+      message: "Remaining leave days cannot be negative"
+    )
+    |> unique_constraint([:user_id, :leave_type_id],
+      name: :leave_balances_user_id_leave_type_id_index
+    )
+  end
+
+  def create(attrs) do
+    case get_leave_balance(attrs.user_id, attrs.leave_type_id) do
+      nil ->
+        %__MODULE__{}
+        |> changeset(attrs)
+        |> Repo.insert()
+
+      _existing_record ->
+        {:error, "Leave balance for this leave type already exists"}
+    end
+  end
+
+  def get_leave_balance(user_id, leave_type_id) do
+    from(lb in __MODULE__,
+      where: lb.user_id == ^user_id and lb.leave_type_id == ^leave_type_id,
+      select: lb
+    )
+    |> Repo.one()
+  end
+
+  def update_leave_balance(user_id, leave_type_id, new_days) do
+    case get_leave_balance(user_id, leave_type_id) do
+      nil ->
+        {:error, "Leave balance not found"}
+
+      leave_balance ->
+        leave_balance
+        |> changeset(%{remaining_days: new_days})
+        |> Repo.update()
+    end
+  end
+
+  def list_user_leave_balances(user_id) do
+    from(lb in __MODULE__,
+      where: lb.user_id == ^user_id and is_nil(lb.deleted_at),
+      select: lb
+    )
+    |> Repo.all()
+  end
+
+  def deduct_leave_days(user_id, leave_type_id, days) do
+    case get_leave_balance(user_id, leave_type_id) do
+      nil ->
+        {:error, "Leave balance not found"}
+
+      %__MODULE__{remaining_days: remaining} = leave_balance when remaining >= days ->
+        leave_balance
+        |> changeset(%{remaining_days: remaining - days})
+        |> Repo.update()
+
+      _ ->
+        {:error, "Not enough leave balance"}
+    end
   end
 end
---
+++ lib/ims_web/live/profile_live.ex
@@ -0,0 +1,170 @@
+defmodule ImsWeb.UserProfileLive do
+  # alias Ims.Inventory
+  use ImsWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    # Mock Data
+    current_user = socket.assigns.current_user |> Ims.Repo.preload([:department, leave_balances: :leave_type])
+
+    annual_remaining_days =
+    case Enum.find(current_user.leave_balances, fn balance ->
+      balance.leave_type.name == "annual"
+    end) do
+      nil -> Decimal.new("0.00") # Default value if no annual leave balance is found
+      annual_balance -> annual_balance.remaining_days
+    end
+
+    user_info = %{
+      avatar_url: "https://via.placeholder.com/150",
+      name: "#{current_user.first_name}",
+      department: "#{current_user.department.name}",
+      email: "#{current_user.email}",
+      phone: "#{current_user.msisdn}" || "N/A",
+      assignments: [],
+      returned_devices: [],
+      leave_days: annual_remaining_days,
+      lost_devices: []
+    }
+    #   assignments:
+    #     Ims.Inventory.get_assigned_user_devices(socket.assigns.current_user),
+    #   returned_devices:
+    #     Ims.Inventory.get_user_returned_devices(socket.assigns.current_user),
+    #   leave_days: annual_remaining_days,
+    #   lost_devices:
+    #     Inventory.get_lost_devices(socket.assigns.current_user)
+
+    # }
+
+    {:ok, assign(socket, user_info: user_info)}
+  end
+
+  @impl true
+  def handle_event("apply_for_leave", _params, socket) do
+    {:noreply, push_navigate(socket, to: "/leave_applications/new")}
+  end
+
+  @impl true
+  def handle_event("request_for_device", _params, socket) do
+    {:noreply, push_navigate(socket, to: "/requests/new")}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="container mx-auto p-8 max-w-5xl bg-white rounded-lg shadow-md">
+      <div class="flex items-center mb-6">
+        <div>
+          <h1 class="text-3xl font-bold text-gray-800">User Profile</h1>
+          <p class="text-lg text-gray-600">Welcome back, <strong><%= @user_info.name %></strong>!</p>
+        </div>
+      </div>
+      <!-- Personal Information -->
+      <div class="bg-gray-50 p-6 rounded-lg mb-6 shadow">
+        <div class="flex justify-between items-start">
+          <div>
+            <h2 class="text-2xl font-semibold text-gray-700 mb-4">Personal Information</h2>
+            <p class="text-gray-800 mb-2"><strong>Name:</strong> <%= @user_info.name %></p>
+            <p class="text-gray-800 mb-2">
+              <strong>Department:</strong> <%= @user_info.department %>
+            </p>
+            <p class="text-gray-800 mb-2"><strong>Email:</strong> <%= @user_info.email %></p>
+            <p class="text-gray-800 mb-4"><strong>Phone:</strong> <%= @user_info.phone %></p>
+          </div>
+          <div class="ml-6 p-4 bg-gray-100 rounded-lg shadow">
+            <h3 class="text-xl font-semibold text-gray-700 mb-4">Leave Details</h3>
+            <p class="text-gray-800 mb-4">
+              <strong>Available Leave Days:</strong> <%= @user_info.leave_days %>
+            </p>
+            <button
+              class="bg-blue-500 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-600"
+              phx-click="apply_for_leave"
+            >
+              Apply for Leave
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- Assignments -->
+      <div class="bg-gray-50 p-6 rounded-lg mb-6 shadow">
+        <div class="flex justify-between items-center mb-4">
+          <h2 class="text-2xl font-semibold text-gray-700">Assignments</h2>
+          <button
+            class="bg-blue-500 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-600"
+            phx-click="request_for_device"
+          >
+          Request for Device
+          </button>
+
+        </div>
+        <table class="w-full table-auto border-collapse rounded-lg overflow-hidden">
+          <thead>
+            <tr class="bg-gray-200">
+              <th class="border px-4 py-2 text-left">Device ID</th>
+              <th class="border px-4 py-2 text-left">Device Name</th>
+              <th class="border px- 4 py-2 text-left">Serial Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            <%= for assignment <- @user_info.assignments do %>
+              <tr class="hover:bg-gray-100">
+                <td class="border px-4 py-2 text-center"><%= assignment.id %></td>
+                <td class="border px-4 py-2"><%= assignment.name %></td>
+                <td class="border px-4 py-2"><%= assignment.serial_number %></td>
+              </tr>
+            <% end %>
+          </tbody>
+        </table>
+      </div>
+      <!-- Returned Devices -->
+      <div class="bg-gray-50 p-6 rounded-lg mb-6 shadow">
+        <h2 class="text-2xl font-semibold text-gray-700">Returned Devices</h2>
+
+        <table class="w-full table-auto border-collapse rounded-lg overflow-hidden">
+          <thead>
+            <tr class="bg-gray-200">
+              <th class="border px-4 py-2 text-left">Device ID</th>
+              <th class="border px-4 py-2 text-left">Device Name</th>
+              <th class="border px- 4 py-2 text-left">Serial Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            <%= for returned_device <- @user_info.returned_devices do %>
+              <tr class="hover:bg-gray-100">
+                <td class="border px-4 py-2 text-center"><%= returned_device.id %></td>
+
+                <td class="border px-4 py-2"><%= returned_device.device.device_name.name %></td>
+                <td class="border px-4 py-2"><%= returned_device.device.serial_number %></td>
+              </tr>
+            <% end %>
+          </tbody>
+        </table>
+      </div>
+      <!-- Lost Devices -->
+      <div class="bg-gray-50 p-6 rounded-lg shadow">
+        <h2 class="text-2xl font-semibold text-gray-700 mb-4">Lost Devices</h2>
+        <table class="w-full table-auto border-collapse rounded-lg overflow-hidden">
+          <thead>
+            <tr class="bg-gray-200">
+              <th class="border px-4 py-2 text-left">Device ID</th>
+              <th class="border px-4 py-2 text-left">Serial Number</th>
+              <th class="border px-4 py-2 text-left">Device Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <%= for device <- @user_info.lost_devices do %>
+              <tr class="hover:bg-gray-100">
+                <td class="border px-4 py-2 text-center"><%= device.id %></td>
+                <td class="border px-4 py-2"><%= device.serial_number %></td>
+                <td class="border px-4 py-2"><%= device.name %></td>
+
+                <%!-- <td class="border px-4 py-2 text-center"><%= device.date_lost %></td> --%>
+              </tr>
+            <% end %>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    """
+  end
+end
